1b7022935948b2de263bc854848cc84c4e4caa84
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2023-07-14 19:11:23 +0700

    Finish pass-word generator

---
+++ PassWord.py
@@ -0,0 +1,19 @@
+import random
+import string
+import pyperclip
+
+
+def generate_password(length=16):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
+
+
+def main():
+    password = generate_password()
+    print("Generated Password:", password)
+    pyperclip.copy(password)
+
+
+if __name__ == "__main__":
+    main()
